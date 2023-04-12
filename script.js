f3a8d15745f570f3692a1fca07f56ebcad0c4301
Adithya <adikrishna1972@gmail.com>
2023-04-13 01:51:12 +0530

    Created Home Section by adding slider images, stopped @00:30

---
+++ script.js
@@ -0,0 +1,13 @@
+// Home Carousel
+
+$('.home-carousel').owlCarousel({
+    Loop:true,
+    margin:0,
+    dots:false,
+    smartSpeed:700,
+    responsive: {
+        0: {
+            items:1
+        }
+    }
+})
