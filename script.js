d4a842bb83624ef07fb6f890df3f702593eb1d5b
Adithya <adikrishna1972@gmail.com>
2023-04-14 07:57:10 +0530

    created slider @ home section, stopped at 00:34

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Home Carousel
 
-$('.home-carousel').owlCarousel({
+$('.home-carousel').owlCarousel({   //used to create responsive carousel sliders... its an enables jquery plugin
     Loop:true,
     margin:0,
     dots:false,
@@ -10,4 +10,29 @@ $('.home-carousel').owlCarousel({
             items:1
         }
     }
+})
+
+// Custom carousel buttons
+let wrapper = document.querySelectorAll(".wrapper");
+
+wrapper.forEach(option => {
+    option.addEventListener("click", function clickhandler(e) {
+        // if button is clicked contacts 'carousel-next-btn' class
+        console.log("running 0");
+        if(e.target.classList.contains("carousel_next_btn")) {
+            //gets data
+            console.log("running");
+            const target = e.target.getAttribute("data-target");
+            var owl = $(target);
+            console.log(owl);
+            owl.owlCarousel()
+
+            //goto next item
+            owl.trigger('next.owl.carousel');
+        }
+        else {
+            console.log("Entered else");
+            return
+        }
+    })
 })
