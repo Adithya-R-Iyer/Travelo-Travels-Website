c33e3672604b8c523c6b4360ec9be5c3030b3799
Adithya <adikrishna1972@gmail.com>
2023-04-15 23:13:32 +0530

    added responsive navbar and completed 'destinations' section

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
-// Home Carousel
+// ******************  code to toggle navmenu ********************
 
-$('.home-carousel').owlCarousel({   //used to create responsive carousel sliders... its an enables jquery plugin
+const navMenu = document.getElementById("nav_menu"),
+      navToggle = document.getElementById("nav_toggle"),
+      navClose = document.getElementById("nav_close");
+
+//   showing menu
+if(navToggle) {
+    navToggle.addEventListener('click', ()=> {
+        navMenu.classList.add('show_menu');
+    })
+}
+
+//   hiding menu 
+if(navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show_menu');
+    })
+}
+
+// fixing navbar
+window.addEventListener('scroll', function() {
+    let navWrapper = this.document.getElementById('nav_wrapper');
+    navWrapper.classList.toggle('fixed', this.window.scrollY >100)
+})
+
+// hiding navbar on scroll down
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollpos = window.pageYOffset;
+
+    // show nav when scrolled upward
+    if(prevScrollpos > currentScrollpos) {
+        document.getElementById('nav_wrapper').style.top = "0";
+    }// hide nav when scrolled downward
+    else {
+        document.getElementById('nav_wrapper').style.top = "-100%";
+    }
+    prevScrollpos=currentScrollpos;
+}
+
+
+// Home Carousel
+$('.home-carousel').owlCarousel({   //used to create responsive carousel sliders... its an enabled jquery plugin
     Loop:true,
     margin:0,
     dots:false,
     smartSpeed:700,
+    // autoplay:true,
+    // autoplayTimeout: 8000,
     responsive: {
         0: {
             items:1
@@ -16,23 +60,44 @@ $('.home-carousel').owlCarousel({   //used to create responsive carousel sliders
 let wrapper = document.querySelectorAll(".wrapper");
 
 wrapper.forEach(option => {
-    option.addEventListener("click", function clickhandler(e) {
+    option.addEventListener('click', function clickhandler(e) {
         // if button is clicked contacts 'carousel-next-btn' class
-        console.log("running 0");
         if(e.target.classList.contains("carousel_next_btn")) {
             //gets data
-            console.log("running");
             const target = e.target.getAttribute("data-target");
             var owl = $(target);
-            console.log(owl);
-            owl.owlCarousel()
+            owl.owlCarousel();
 
             //goto next item
             owl.trigger('next.owl.carousel');
         }
+        else if(e.target.classList.contains("carousel_prev_btn")) {
+            //gets data
+            const target = e.target.getAttribute("data-target");
+            var owl = $(target);
+            owl.owlCarousel();
+
+            //goto next item
+            owl.trigger('prev.owl.carousel');
+        }
         else {
-            console.log("Entered else");
             return
         }
     })
+})
+
+
+// Destination Carousel
+$('.destination-carousel').owlCarousel({   //used to create responsive carousel sliders... its an enabled jquery plugin
+    Loop:true,
+    margin:35,
+    dots:false,
+    smartSpeed:700,
+    // autoplay:true,
+    // autoplayTimeout: 8000,
+    responsive: {
+        0: {
+            items:1
+        }
+    }
 })
